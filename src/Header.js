709f4ac4ffee995a60a2daec6aa23ca42b8358d6
ahmedyounis253 <amy2532001@gmail.com>
2021-09-08 02:16:54 +0200

    creating React components

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+const Header = props =>{
+    const {branding}=props;
+     return (
+
+        <nav className='nav navbar navbar-expand-lg navbar-dark bg-black mb-3 py-0'>
+            <div className='container'>
+            <a className='navbar-brand'>{branding}</a>
+            <div>
+                <ul className="navbar-nav mr-auto">
+                    <li className='nav-item'>
+                        <a href='' className='nav-link'>home </a>
+                    </li>
+                    <li>
+
+
+                        </li>
+
+                </ul>
+
+            </div>
+            </div>
+        </nav>
+     )
+     
+     
+};
+Header.propTypes={
+
+    branding:PropTypes.string.isRequired
+};
+
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Contact from './contact';
 import reportWebVitals from './reportWebVitals';
-
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Header branding='contact manager'/>
+    <div className='container'>
+    <Contact name='ahmed' Email='younis@mail.com' phone='01098015491'/>
+    <Contact name='rami' Email='rami@mail.com' phone='012435t342234'/>
+    </div>
+  </React.StrictMode>
+,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
